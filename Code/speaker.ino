7f7b3d37ff44de9a991d7489fa2e50b49239e3d8
Ivan Zheng <ivz11@pitt.edu>
2026-04-21 20:51:05 -0400

    refactor setup sequence and remove playTrack function

--- Code/speaker.ino
+++ Code/speaker.ino
@@ -5,24 +5,19 @@
 #define Acknowledge 0x00
 
 void setup() {
+
   Serial.begin(9600);
-  delay(1000);
   execute_CMD(0x3F, 0, 0);  // init
-  delay(500);
-  setVolume(5);
-  delay(500);
-  playTrack(1, 10000);
-  playTrack(2, 45000);
-  playTrack(1, 30000);
+  delay(1000);
+  setVolume(10);
+  delay(1000);
+  execute_CMD(0x08, 0, 1);
+  delay(100);
+
 }
 
 void loop() {
   // nothing needed — module loops tracks automatically
-}
-
-void setVolume(int volume) {
-  execute_CMD(0x06, 0, volume);
-  delay(500);
 }
 
 void execute_CMD(byte CMD, byte Par1, byte Par2) {
@@ -37,8 +32,6 @@ void execute_CMD(byte CMD, byte Par1, byte Par2) {
     Serial.write(Command_line[k]);
   }
 }
-
-void playTrack(int track, unsigned long duration) {
-  execute_CMD(0x03, 0, track);
-  delay(duration);  // wait for track to finish
+void setVolume(int volume) {
+  execute_CMD(0x06, 0, volume);
 }
